8222598dd71009f8da3cc6b533301fc0270c060e
Kevin Palermo <kevpalermo@gmail.com>
2018-07-15 05:35:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // write your code below!
-function happyHolidays(string) {
+function happyHolidays() {
   return "Happy holidays!"
 }
-function happyHolidaysTo(string, name) {
+function happyHolidaysTo(greeting, name) {
   return `Happy holidays, ${name}!`
 }
